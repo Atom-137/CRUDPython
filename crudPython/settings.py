d4f7bcb31596ce3bf12be68c70d75cd965eb9d5f
ss_Muss <jafirgarcia@gmail.com>
2024-03-07 23:59:15 -0600

    conexion a bd

--- crudPython/settings.py
+++ crudPython/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'crudPython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postresql',
+        'NAME': BASE_DIR / 'tasksdb',
+        'USER': 'postgres',
+        'PASSWORD':'contrasenia123',
+        'HOST'    : '127.0.0.1',
+        'PORT'    : '5432'
     }
 }
 
